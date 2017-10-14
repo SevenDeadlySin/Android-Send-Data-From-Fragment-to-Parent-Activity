15effca3c2e4d4a0501b7207bda1c257c6a00c9a
BamBo <raksameth@gmail.com>
2017-10-14 15:36:22 +0700

    .

--- App/app/src/main/java/com/example/raksa/senddatafromparentactivitytofragment/MainActivity.java
+++ App/app/src/main/java/com/example/raksa/senddatafromparentactivitytofragment/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.raksa.senddatafromparentactivitytofragment;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView = (TextView) findViewById(R.id.textView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BlankFragment fragment = new BlankFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("name","Raksa");
+
+        fragment.setArguments(bundle);
+        
+
     }
 }
